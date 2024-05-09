44f5cbed41356f622558987ed6fb7ca7612f2b7b
TheEnumbin <enumbin@gmail.com>
2024-05-09 21:45:16 +0600

    added table

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Table from "./components/Table";
 import "./App.css";
 import "./style.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="to-do-wrapper bg-white">
       <Header></Header>
+      <Table></Table>
     </div>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,20 @@
 const Table = () => {
-  return <table></table>;
+  return (
+    <table className="text-black">
+      <tr>
+        <th>Task Id</th>
+        <th>Task Name</th>
+        <th>Status</th>
+        <th>Action</th>
+      </tr>
+      <tr>
+        <td>01</td>
+        <td>Eat</td>
+        <td>Done</td>
+        <td>Edit</td>
+      </tr>
+    </table>
+  );
 };
 
 export default Table;
