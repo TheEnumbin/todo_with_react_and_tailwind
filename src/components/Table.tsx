c0d96a12090d949aca2ddd52ec1c91198f6a1d89
TheEnumbin <enumbin@gmail.com>
2024-05-10 13:17:42 +0600

    table layout came

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,18 +1,20 @@
 const Table = () => {
   return (
     <table className="text-black border-2 w-[100%]">
-      <tr>
-        <th>Task Id</th>
-        <th>Task Name</th>
-        <th>Status</th>
-        <th>Action</th>
-      </tr>
-      <tr>
-        <td>01</td>
-        <td>Eat</td>
-        <td>Done</td>
-        <td>Edit</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th className="w-[10%]">Task Id</th>
+          <th>Task Name</th>
+          <th>Status</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>01</td>
+          <td>Eat</td>
+          <td>Done</td>
+          <td>Edit</td>
+        </tr>
+      </tbody>
     </table>
   );
 };
