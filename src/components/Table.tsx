a3d97944a36d5de7179a0214cc816429f27a6de8
TheEnumbin <enumbin@gmail.com>
2024-12-23 23:48:23 +0600

    working on dnd update

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { TaskList } from "./partial/TaskList";
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, closestCenter } from "@dnd-kit/core";
 import {
   SortableContext,
   verticalListSortingStrategy,
@@ -10,6 +10,18 @@ interface TableProps {
   updateStatus: (checked: boolean) => void;
 }
 const Table = ({ tasks, updateStatus }: TableProps) => {
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setTasks((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
   let tasks_arr = ["Eat", "Code", "Sleep", "Repeat"];
   return (
     <table className="text-black border-2 w-[100%]">
@@ -20,7 +32,10 @@ const Table = ({ tasks, updateStatus }: TableProps) => {
           <th>Status</th>
           <th>Action</th>
         </tr>
-        <DndContext>
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+        >
           <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
             <TaskList tasks={tasks} updateStatus={updateStatus}></TaskList>
           </SortableContext>
